94f697bac110a1dde0e71634ca9a430e991118cb
Austen Turner <austenpturner@msn.com>
2024-11-04 13:33:38 -0800

    update vite config base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/healthcare-website/",
+  base: "/healthcare-website-mockup/",
   build: {
     outDir: "dist",
   },
