c18bb53574f111cd058e21331f325763bc1df977
Austen Turner <austenpturner@msn.com>
2024-11-26 09:16:02 -0800

    add contact section corners, clean up button scss

--- src/components/contactSection/index.jsx
+++ src/components/contactSection/index.jsx
@@ -5,27 +5,44 @@ import { ContactForm } from "../contactForm";
 
 export default function ContactSection() {
   return (
-    <section className={styles.contact}>
+    <section className={styles.contactSection}>
       <div className={styles.contactContainer}>
-        <div className={styles.content}>
-          <h4>{contactSectionContent.formHeader}</h4>
-          <div>
-            <p>{contactSectionContent.contactHeader}</p>
-            {contact.map((method) => {
-              return (
-                <a
-                  href={method.href}
-                  key={method.id}
-                  className={styles.contactLink}
-                >
-                  <method.icon />
-                  <span>{method.value}</span>
-                </a>
-              );
-            })}
-          </div>
+        <div className={styles.cornerContainer}>
+          <span className={`${styles.corner} ${styles.topLeftCorner}`}></span>
+          <span className={`${styles.corner} ${styles.topRightCorner}`}></span>
         </div>
-        <ContactForm />
+
+        <div className={styles.contentContainer}>
+          <div className={styles.headerContainer}>
+            <h4>{contactSectionContent.formHeader}</h4>
+            <div>
+              <p>{contactSectionContent.contactHeader}</p>
+              {contact.map((method) => {
+                return (
+                  <a
+                    href={method.href}
+                    key={method.id}
+                    className={styles.contactLink}
+                  >
+                    <method.icon />
+                    <span>{method.value}</span>
+                  </a>
+                );
+              })}
+            </div>
+          </div>
+
+          <ContactForm />
+        </div>
+
+        <div className={styles.cornerContainer}>
+          <span
+            className={`${styles.corner} ${styles.bottomLeftCorner}`}
+          ></span>
+          <span
+            className={`${styles.corner} ${styles.bottomRightCorner}`}
+          ></span>
+        </div>
       </div>
     </section>
   );
