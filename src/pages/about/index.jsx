524b8593890613c0c47b8622c63dd0d900a89806
Austen Turner <austenpturner@msn.com>
2024-11-27 11:03:51 -0800

    add mission and value statements to about page

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,6 +1,7 @@
 import "./styles.scss";
 import Blocks from "../../components/blocks";
 import { blockContent } from "../../config/about";
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa6";
 
 export default function AboutPage() {
   return (
@@ -10,6 +11,28 @@ export default function AboutPage() {
       </div>
       <div className="content-container">
         <Blocks content={blockContent} location="about" />
+
+        <div className="mission-values-container">
+          <div className="mission-container">
+            <h2>Our Mission:</h2>
+            <div className="quote-container">
+              <FaQuoteLeft />
+              {/* <div> */}
+              <p>
+                To provide exceptional care that inspires growth and healing.
+              </p>
+              <span></span>
+              {/* </div> */}
+              <FaQuoteRight />
+            </div>
+          </div>
+
+          <div className="value-container">
+            <h2>Our Values:</h2>
+            <p>Compassion, Inclusivity, & Professionalism</p>
+            <span></span>
+          </div>
+        </div>
       </div>
     </div>
   );
